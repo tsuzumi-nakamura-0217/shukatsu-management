b8b4fe27c01c6b1f02ccf1ab33fbe9354613dbf8
Tsuzumi Nakamura <smallkabutomsi@gmail.com>
2026-03-28 13:05:50 +0900

    fix(config): next.config.ts の experimental オプションを serverActions.allowedOrigins に修正

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    allowedDevOrigins: [
-      "localhost:3000",
-      "127.0.0.1:3000",
-      "192.168.11.27:3000" // PCやスマホ等、同一ネットワーク内からアクセスするための許可
-    ]
+    serverActions: {
+      allowedOrigins: [
+        "localhost:3000",
+        "127.0.0.1:3000",
+        "192.168.11.27:3000" // PCやスマホ等、同一ネットワーク内からアクセスするための許可
+      ]
+    }
   }
 };
 
